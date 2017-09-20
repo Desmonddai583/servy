e874f7dbd397275117a92d46a97ea6926055b979
desmonddai583 <desmonddai583@gmail.com>
2017-09-20 18:33:38 +0800

    module 18

--- lib/servy/parser.ex
+++ lib/servy/parser.ex
@@ -3,9 +3,9 @@ defmodule Servy.Parser do
   alias Servy.Conv
 
   def parse(request) do
-    [top, params_string] = String.split(request, "\n\n")
+    [top, params_string] = String.split(request, "\r\n\r\n")
 
-    [request_line | header_lines ] = String.split(top, "\n")
+    [request_line | header_lines ] = String.split(top, "\r\n")
 
     [method, path, _] = String.split(request_line, " ")
 
@@ -36,6 +36,17 @@ defmodule Servy.Parser do
 
   # def parse_headers([], headers), do: headers
 
+  @doc """
+  Parse the given param string of form `key1=value&key2=value2`
+  into a map with corresponding keys and values
+
+  ## Examples
+      iex> param_string = "name=Baloo&type=Brown"
+      iex> Servy.Parser.parse_params("application/x-www-form-urlencoded", param_string)
+      %{"name" => "Baloo", "type" => "Brown"}
+      iex> Servy.Parser.parse_params("multipart/form-data", param_string)
+      %{}
+  """
   def parse_params("application/x-www-form-urlencoded", params_string) do
     params_string |> String.trim |> URI.decode_query
   end
